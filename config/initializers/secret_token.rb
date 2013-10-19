78b8ff1854aeebde0aeb660ec3dd31f1e831e49f
Elias Tsironis <eliastsirwnhs@gmail.com>
2013-10-19 13:35:41 +0300

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Norouko::Application.config.secret_token = 'a70ad249b11fe15c24c6f1772d32fbe9fc2f8ac8567bb7fb5dd9bbc0b192239ef75cfd9ed5f445fb4be98ece90e556c9dd2f9ba1af04e0e553ff5abaa402c292'
